1900b04646b49ea9cdd5228f10b05933b7b04b63
dczheng <dczheng21@outlook.com>
2019-11-07 14:31:07 +0800

    update

--- FoF/fof.c
+++ FoF/fof.c
@@ -7,28 +7,16 @@
 
 // a implementation of 2D fof.
 
-int *Next, *Head, *Tail, *Len, Width, Height, FoFN, *FoFMap, *Flag,
-#ifdef HILBERT_INDEX
-     nidx_tot, bits, nidx_per_task,
-#endif
-     Xmin, Ymin, Xmax, Ymax, DX, DY;
-
+int *Next, *Head, *Tail, *Len, Width, Height, FoFN;
+int *FoFMap;
 //#define FOF_SINGLE_DEBUG
-#define FOF_SINGLE_DEBUG2
-
-#ifdef FOF_SINGLE_DEBUG2 
-    int PN;
-#endif
-
-void fof_single_finder( int p0, int mode ) {
+void fof_single_finder( int p0 ) {
 
     int  i, j, x0, y0;
     int p1, l, s, ss, p_next;
 
     if ( FoFMap[p0] == 0 )
         return;
-
-    Flag[p0] = 1;
 
     x0 = p0 % Width;
     y0 = p0 / Width;
@@ -52,21 +40,12 @@ void fof_single_finder( int p0, int mode ) {
                  FoFMap[p1]  == 0 )
                 continue;
 
-            if ( mode )
-                if ( y0+i<Ymin ||
-                     y0+i>Ymax-1 ||
-                     x0+j<Xmin ||
-                     x0+j>Xmax-1 )
-                    continue;
-
 #ifdef FOF_SINGLE_DEBUG
             printf( "1\n" );
             fflush( stdout );
 #endif
             if ( Head[p1] == Head[p0] )
                 continue;
-
-            Flag[p1] = 1;
 
             if ( Len[Head[p1]] > Len[Head[p0]] ) {
                 l = p1;
@@ -86,9 +65,6 @@ void fof_single_finder( int p0, int mode ) {
             Len[Head[l]] += Len[Head[s]];
             Len[Head[s]] = 1;
 
-#ifdef FOF_SINGLE_DEBUG2
-        PN++;
-#endif
             ss = Head[s];
             do{
                 /*
@@ -113,25 +89,13 @@ void fof_single_finder( int p0, int mode ) {
 #endif
 
     if ( p_next >= 0 )
-        fof_single_finder( p_next, mode );
+        fof_single_finder( p_next );
 
-}
-
-int hidx_compare( const void *a, const void *b ) {
-    return ( *((int*)a) > *((int*)b) ) ? 1 : -1;
 }
 
 void fof_init( int *map, int W, int H ) {
 
     int i;
-
-//#define FOF_INIT_DEBUG
-
-#ifdef FOF_INIT_DEBUG
-    FILE *fd;
-    char buf[100];
-    int p[2];
-#endif
 
     Width = W;
     Height = H;
@@ -142,51 +106,13 @@ void fof_init( int *map, int W, int H ) {
     Head = malloc( sizeof(int) * FoFN );
     Len = malloc( sizeof(int) * FoFN );
     Tail = malloc( sizeof(int) * FoFN );
-    Flag = malloc( sizeof(int) * FoFN );
 
     for( i=0; i<FoFN; i++ ) {
         Next[i] = -1;
         Head[i] = i;
         Tail[i] = i;
-        Len[i]  = 1;
-        Flag[i] = 0;
+        Len[i] = 1;
     }
-
-    if ( NTask == 1 ) 
-        return;
-
-#ifdef HILBERT_INDEX
-    for( bits=1; (1<<bits)<sqrt(NTask); bits++ );
-    nidx_tot = 1<<(bits*2);
-    nidx_per_task = nidx_tot / NTask;
-    if ( nidx_tot % NTask != 0 )
-        nidx_per_task ++;
-    DX = Width  / (1<<bits);
-    DY = Height / (1<<bits);
-
-#ifdef FOF_INIT_DEBUG
-    printf( "[%i] bits: %i [%i], nidx_tot: %i, nidx_per_task: %i, "
-            "DXY: (%i, %i)\n",
-    ThisTask,
-    bits, 1<<bits, nidx_tot, nidx_per_task, DX, DY );
-    sprintf( buf, "p_%03i.dat", ThisTask );
-    fd = fopen( buf, "w" );
-    for( i=0; i<nidx_tot; i++ ) {
-        if ( i / nidx_per_task != ThisTask )
-            continue;
-        hilbert_index_inv( &i, p, 2, bits );
-        fprintf( fd, "%i %i\n", p[0], p[1] );
-    }
-#endif
-#ifdef FOF_INIT_DEBUG
-    fclose( fd );
-#endif
-
-#else
-    DX = Width / NTask;
-    DY = Height / NTask;
-#endif
-
 }
 
 void fof_reset() {
@@ -197,7 +123,6 @@ void fof_reset() {
         Head[i] = i;
         Tail[i] = i;
         Len[i] = 1;
-        Flag[i] = 0;
     }
 }
 
@@ -207,7 +132,6 @@ void fof_free() {
     free( Head );
     free( Tail );
     free( Len );
-    free( Flag );
 
 }
 
@@ -235,162 +159,10 @@ void fof_sort () {
     free( tmp );
 }
 
-typedef struct FoFData{
-    int Next, Len, Head, Tail, index;
-}FoFData;
-
 void fof() {
-    int p, k, i, j, n_local, *n_all, *n_dis, n_tot, n_pad;
-    FoFData *FDat, *FDat_tot;
-    n_pad = 0;
-#ifdef HILBERT_INDEX
-    int pp[2];
-#endif
-
-#define FOF_DEBUG
-#ifdef FOF_SINGLE_DEBUG2
-    PN = 0;
-#endif
-    if ( NTask == 1 ) {
-        for( p=0; p<FoFN; p++ )
-            fof_single_finder( p, 0 );
-#ifdef FOF_SINGLE_DEBUG2
-    printf( "[%i] PN: %i\n", ThisTask, PN );
-#endif
-        return;
+    int p;
+    for( p=0; p<FoFN; p++ ) {
+        fof_single_finder(  p );
     }
-
-#ifdef HILBERT_INDEX
-    for( k=0, n_local=0; k<nidx_tot; k++ ) {
-        if ( k / nidx_per_task != ThisTask )
-            continue;
-        hilbert_index_inv( &k, pp, 2, bits );
-        Xmin = pp[0] * DX;
-        Xmax = Xmin + DX-n_pad; 
-        Ymin = pp[1] * DY;
-        Ymax = Ymin + DY-n_pad; 
-#else
-    for( k=0, n_local=0; k<NTask; k++ ){
-        if ( k != ThisTask )
-            continue;
-        Xmin = DX * k;
-        Xmax = Xmin + DX-n_pad;
-        Ymin = 0;
-        Ymax = Height;
-#endif
-        n_local += ( Xmax-Xmin ) * ( Ymax-Ymin );
-#ifdef FOF_DEBUG
-        printf( "[%i] X:(%i, %i), Y:(%i, %i)\n", ThisTask,
-                Xmin, Xmax, Ymin, Ymax );
-#endif
-        for( i=Ymin; i<Ymax; i++ )
-            for ( j=Xmin; j<Xmax; j++ ){
-                p = i * Width + j; 
-                fof_single_finder( p, 1 );
-            }
-    }
-
-#ifdef FOF_SINGLE_DEBUG2
-    printf( "[%i] PN: %i\n", ThisTask, PN );
-#endif
-#ifdef FOF_DEBUG
-    printf( "[%i] n_local: %i\n", ThisTask, n_local );
-#endif
-
-    FDat = malloc( sizeof(FoFData) * n_local );
-    if ( NULL == FDat ) {
-        printf( "malloc error [%li]\n", sizeof(FoFData)*n_local );
-        exit(0);
-    }
-
-#ifdef HILBERT_INDEX
-    for( k=0, n_local=0; k<nidx_tot; k++ ) {
-        if ( k / nidx_per_task != ThisTask )
-            continue;
-        hilbert_index_inv( &k, pp, 2, bits );
-        Xmin = pp[0] * DX;
-        Xmax = Xmin + DX-n_pad; 
-        Ymin = pp[1] * DY;
-        Ymax = Ymin + DY-n_pad; 
-#else
-    for( k=0, n_local=0; k<NTask; k++ ){
-        if ( k != ThisTask )
-            continue;
-        Xmin = DX * k;
-        Xmax = Xmin + DX-n_pad;
-        Ymin = 0;
-        Ymax = Height;
-#endif
-        for( i=Ymin; i<Ymax; i++ )
-            for ( j=Xmin; j<Xmax; j++ ){
-                p = i * Width + j; 
-                if ( p < 0       ||
-                     p > FoFN-1 )
-                    continue;
-
-                if ( Flag[p] ){
-#ifndef HILBERT_INDEX
-                    if ( (p%Width) / DX != ThisTask ) {
-                        printf( "[%i] can't be, [%i]\n",
-                        ThisTask, (p%Width)/DX );
-                        exit(1);
-                    }
-#endif
-                    FDat[n_local].Next = Next[p];
-                    FDat[n_local].Head = Head[p];
-                    FDat[n_local].Tail = Tail[p];
-                    FDat[n_local].Len  = Len[p];
-                    FDat[n_local].index  = p;
-                    n_local ++;
-                }
-            }
-
-    }
-
-    n_all = malloc( sizeof(int) * NTask );
-    n_dis = malloc( sizeof(int) * NTask );
-    MPI_Gather( &n_local, 1, MPI_INT, n_all, 1, MPI_INT, 0, MPI_COMM_WORLD );
-    if ( ThisTask == 0 ){
-        for( i=0, n_tot=0; i<NTask; i++ ) {
-            n_tot += n_all[i];
-            if (i==0 )
-                n_dis[i] = 0;
-            else
-                n_dis[i] = n_dis[i-1] + n_all[i-1];
-        }
-
-#ifdef FOF_DEBUG
-        printf( "n_tot: %i\n", n_tot );
-#endif
-        FDat_tot = malloc( sizeof(FoFData) * n_tot );
-    }
-
-    MPI_Gatherv( FDat, n_local, MPI_INT, 
-                 FDat_tot, n_all, n_dis, MPI_INT, 0, MPI_COMM_WORLD );
-    free( n_all );
-    free( n_dis );
-    free( FDat );
-
-    if ( ThisTask )
-        return;
-
-    for( i=0; i<n_tot; i++ ) {
-        p = FDat_tot[i].index;
-#ifdef HILBERT_INDEX
-#else
-        if ( (p%Width)/DX == 0 )
-            continue;
-#endif
-        if ( Flag[p] == 1 ) {
-            printf( "can't be\n" );
-            exit(1);
-        }
-        Next[p] = FDat_tot[i].Next;
-        Head[p] = FDat_tot[i].Head;
-        Tail[p] = FDat_tot[i].Tail;
-        Len[p] = FDat_tot[i].Len;
-
-    }
-
-    //fof_sort();
+    fof_sort();
 }
--- FoF/fof.h
+++ FoF/fof.h
@@ -1,20 +1,5 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "mpi.h"
-#include "limits.h"
-#include "math.h"
-
-#define PARALLELFOF
-
-#ifdef PARALLELFOF
-    extern int NTask, ThisTask;
-#endif
-
-//#define HILBERT_INDEX
-#ifdef HILBERT_INDEX
-#include "hilbert.h"
-#endif
-
 extern int *Next, *Head, *Tail, *Len;
 
 void fof_init( int *map, int W, int H );
